d4c419c2cd75aa79775be6982085e5a75f21ec9b
rezznr <reza.novian.r@gmail.com>
2024-08-26 18:40:48 +0700

    style: Update Homepage styling

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,6 +1,6 @@
 const PokemonList = ({ children }) => {
     return (
-        <div id="pokemon-list" className="grid grid-cols-4 gap-4">
+        <div id="pokemon-list" className="grid grid-cols-4 gap-6">
             {children}
         </div>
     )
--- src/page/Homepage.jsx
+++ src/page/Homepage.jsx
@@ -1,93 +1,86 @@
-import Chosen from "../components/Chosen.jsx";
-import PokemonList from "../components/PokemonList.jsx";
-import {
-  ContainerBody,
-  PokemonOthers,
-  Image,
-  NameP,
-} from "../components/Others.jsx";
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Chosen from '../components/Chosen.jsx';
+import PokemonList from '../components/PokemonList.jsx';
+import { ContainerBody, PokemonOthers, Image, NameP } from '../components/Others.jsx';
 
 const Pages = () => {
-  const [nama, setNama] = useState("Pokemon Name");
-  const [img, setImg] = useState("/images/wheresPokemon.png");
+  const [name, setName] = useState('Pokemon Name');
+  const [img, setImg] = useState('/images/wheresPokemon.png');
   const [data, setData] = useState([]);
-  const token = localStorage.getItem("myToken");
+  const token = localStorage.getItem('myToken');
   const navigate = useNavigate();
 
   const pokemonFetch = async () => {
-    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
-    const data = await response.json();
-    const urls = data.results.map((el) => el.url);
+    try {
+      const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+      const data = await response.json();
+      const urls = data.results.map((el) => el.url);
 
-    const fetchPokemon = async (url) => {
-      const responseAbility = await fetch(url);
-      const resultAbility = await responseAbility.json();
-      return resultAbility;
-    };
-    const fetchArray = urls.map(fetchPokemon);
-    const pokemonData = await Promise.all(fetchArray);
-    const pokemons = pokemonData.map((el) => ({
-      name: el.name,
-      imageUrl: el.sprites.other.dream_world.front_default,
-    }));
-    setData(pokemons);
+      const fetchPokemon = async (url) => {
+        const response = await fetch(url);
+        return await response.json();
+      };
+      const pokemonData = await Promise.all(urls.map(fetchPokemon));
+      const pokemons = pokemonData.map((el) => ({
+        name: el.name,
+        imageUrl: el.sprites.other.dream_world.front_default,
+      }));
+      setData(pokemons);
+    } catch (error) {
+      console.error('Error fetching Pokemon data:', error);
+    }
   };
 
   useEffect(() => {
     pokemonFetch();
   }, []);
 
-  const changes = (nama, img) => {
+  const handleChange = (name, img) => {
     if (token) {
-      setNama(nama);
+      setName(name);
       setImg(img);
       localStorage.setItem(
-        "my-pokemon",
-        JSON.stringify({
-          name: nama,
-          image: img,
-        })
+        'my-pokemon',
+        JSON.stringify({ name, image: img })
       );
-    }
-    if (!token) {
-      alert("You need to Login First");
-      navigate("/login");
+    } else {
+      alert('You need to Login First');
+      navigate('/login');
     }
   };
 
   return (
-    <ContainerBody>
-      <div className="flex">
-        <div>
-          <Chosen>
-            <h2 className="font-bold text-xl mb-2 text-center">
-              Sang Terpilih
-            </h2>
-            <Image size="big" src={img} />
-            <NameP nama={nama} />
-          </Chosen>
-        </div>
-        {data === 0 ? (
-          <h2>Loading...</h2>
-        ) : (
-          <PokemonList>
-            {data.map((poke, idx) => {
-              return (
+    <div>
+      <ContainerBody>
+        <div className="flex gap-5">
+          <div className="relative">
+            <Chosen>
+              <h2 className="mb-2 text-xl font-bold text-center">
+                Sang Terpilih
+              </h2>
+              <Image src={img} />
+              <NameP nama={name} />
+            </Chosen>
+          </div>
+          {data.length === 0 ? (
+            <h2>Loading...</h2>
+          ) : (
+            <PokemonList>
+              {data.map((poke, idx) => (
                 <PokemonOthers
-                  onClick={() => changes(poke.name, poke.imageUrl)}
+                  onClick={() => handleChange(poke.name, poke.imageUrl)}
                   key={idx}
                 >
                   <NameP nama={poke.name} />
                   <Image src={poke.imageUrl} />
                 </PokemonOthers>
-              );
-            })}
-          </PokemonList>
-        )}
-      </div>
-    </ContainerBody>
+              ))}
+            </PokemonList>
+          )}
+        </div>
+      </ContainerBody>
+    </div>
   );
 };
 
